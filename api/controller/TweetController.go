993df0a15eaedd78e47c69ad0e3a0b412e71a7e7
root <root@DESKTOP-KNVCBQI>
2024-01-08 21:32:56 -0300

    feat: Adding a response from SUCCESS to Delete a task

--- api/controller/TweetController.go
+++ api/controller/TweetController.go
@@ -35,6 +35,9 @@ func (t *tweetController) Delete(ctx *gin.Context) {
 	for idx, tweet := range t.tweets {
 		if tweet.ID == id {
 			t.tweets = append(t.tweets[0:idx], t.tweets[idx+1:]...)
+			ctx.JSON(http.StatusOK, gin.H{
+				"OK": "Tweet Deleted",
+			})
 			return
 		}
 	}
