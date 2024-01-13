3c63159696d08f2067f364ffd5cc09a3d1b4c96c
diogenesOliver <diogenes.riboliveira@gmail.com>
2024-01-13 11:31:15 -0300

    wip: Creating test from findAll tweets

---
+++ api/controller/ListTweetController_test.go
@@ -0,0 +1,21 @@
+package controllers
+
+/* import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func FindAllTweets_Test(t *testing.T) {
+	rr := httptest.NewRecorder()
+	FAKE_REQUEST := httptest.NewRequest("GET", "/tweets", nil)
+
+	NewTweetController().FindAll(rr, FAKE_REQUEST)
+	if http.StatusOK != rr.Code {
+		t.Errorf(
+			"Status code expected: %d, got %d",
+			http.StatusOK,
+			rr.Code,
+		)
+	}
+} */
